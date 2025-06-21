fixed masking and teacher forcing

train_scripts/model_classes/CoinWiseCrossAttentionLSTM.py
@@ -37,8 +37,10 @@ class CoinWiseCrossAttentionLSTM(nn.Module):
 
         lstm_stack = torch.stack(lstm_outputs, dim=1)
 
+        attn_mask = nn.Transformer.generate_square_subsequent_mask(self.num_coins).to(x.device)
+
         # apply attention to merged coin pipeline last features
-        attn_out, _ = self.attention(lstm_stack, lstm_stack, lstm_stack)
+        attn_out, _ = self.attention(lstm_stack, lstm_stack, lstm_stack, attn_mask=attn_mask, is_causal=True)
 
         # get the applied attention to the target coin pipeline output
         target_coin = attn_out[:, self.target_coin_index, :]

train_scripts/model_classes/EncoderDecoderGRU.py
@@ -14,9 +14,6 @@ class EncoderGRU(nn.Module):
 class DecoderGRU(nn.Module):
     def __init__(self, output_features, dropout, num_layers, hidden_dim, teacher_forcing_ratio):
         super(DecoderGRU, self).__init__()
-        self.output_features = output_features
-        self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
         self.teacher_forcing_ratio = teacher_forcing_ratio
 
         self.gru = nn.GRU(input_size=output_features, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True, dropout=dropout)
@@ -43,10 +40,10 @@ class DecoderGRU(nn.Module):
 class EncoderDecoderGRU(nn.Module):
     def __init__(self, input_features, output_features, output_window, dropout=0.2, num_layers=3, hidden_dim=128, teacher_forcing_ratio=1.0, target_coin_index=0):
         super(EncoderDecoderGRU, self).__init__()
-        self.encoder = EncoderGRU(input_features, dropout=dropout, num_layers=num_layers, hidden_dim=hidden_dim)
-        self.decoder = DecoderGRU(output_features, dropout=dropout, num_layers=num_layers, hidden_dim=hidden_dim, teacher_forcing_ratio=teacher_forcing_ratio)
-        self.output_window = output_window
+        self.encoder = EncoderGRU(input_features=input_features, dropout=dropout, num_layers=num_layers, hidden_dim=hidden_dim)
+        self.decoder = DecoderGRU(output_features=output_features, dropout=dropout, num_layers=num_layers, hidden_dim=hidden_dim, teacher_forcing_ratio=teacher_forcing_ratio)
         self.output_features = output_features
+        self.output_window = output_window
         self.target_coin_index = target_coin_index
         self.teacher_forcing_ratio = self.decoder.teacher_forcing_ratio
 
@@ -64,7 +61,8 @@ class EncoderDecoderGRU(nn.Module):
         # pass it to decoder with reshaped target if presence
         
         out = self.decoder(last_x, hidden, self.output_window, target)
-        return out.permute(0, 2, 1)
+        out = out.permute(0, 2, 1)
+        return out
 
     def set_teacher_forcing_ratio(self, new_value):
         self.decoder.teacher_forcing_ratio = new_value

train_scripts/model_classes/EncoderDecoderLSTM.py
@@ -15,17 +15,17 @@ class EncoderLSTM(nn.Module):
 class DecoderLSTM(nn.Module):
     def __init__(self, output_features, dropout, num_layers, hidden_dim, teacher_forcing_ratio):
         super(DecoderLSTM, self).__init__()
-        self.output_features = output_features
         self.teacher_forcing_ratio = teacher_forcing_ratio
 
         self.lstm = nn.LSTM(input_size=output_features, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True, dropout=dropout)
-        self.fc = nn.Linear(in_features=hidden_dim, out_features=self.output_features)
+        self.fc = nn.Linear(in_features=hidden_dim, out_features=output_features)
 
     def forward(self, decoder_input, hidden, cell, output_window, target=None):
         outputs = []
 
         for t in range(output_window):
             # pass input to decoder, do the linear projection and store the prediction
+
             out, (hidden, cell) = self.lstm(decoder_input, (hidden, cell))
             pred = self.fc(out.squeeze(1))
             outputs.append(pred.unsqueeze(1))
@@ -52,7 +52,7 @@ class EncoderDecoderLSTM(nn.Module):
     def forward(self, x, target=None):
         if target is not None:
             target = target.permute(0, 2, 1)
-        
+
         # get the last data from training, pass it as the decoder's input
         last_x = x[:, -1, :]
         last_x = last_x[:, self.target_coin_index*self.output_features:(self.target_coin_index+1)*self.output_features]

train_scripts/model_classes/EncoderDecoderTransformer.py
@@ -39,9 +39,6 @@ class EncoderDecoderTransformer(nn.Module):
 
         self.transformer = nn.Transformer(d_model=hidden_dim, nhead=num_heads, num_encoder_layers=num_layers, num_decoder_layers=num_layers, dim_feedforward=512, dropout=dropout, batch_first=True)
 
-    def generate_square_subsequent_mask(self, sz):
-        return torch.triu(torch.ones(sz, sz) * float('-inf'), diagonal=1)
-
     def forward_with_target(self, src, tgt):
         src = src.permute(0, 2, 1)
         src = self.input_proj(src) * math.sqrt(self.hidden_dim)
@@ -51,9 +48,9 @@ class EncoderDecoderTransformer(nn.Module):
         tgt = self.output_proj(tgt) * math.sqrt(self.hidden_dim)
         tgt = self.pos_decoder(tgt)
         
-        tgt_mask = self.generate_square_subsequent_mask(self.output_window).to(src.device)
+        tgt_mask = nn.Transformer.generate_square_subsequent_mask(self.output_window).to(src.device)
         
-        out = self.transformer(src, tgt, tgt_mask=tgt_mask)
+        out = self.transformer(src, tgt, tgt_mask=tgt_mask, tgt_is_causal=True)
         out = self.output_linear(out)
         
         return out.permute(0, 2, 1)
@@ -61,7 +58,7 @@ class EncoderDecoderTransformer(nn.Module):
     def forward_without_target(self, src):
         batch_size = src.size(0)
         src = src.permute(0, 2, 1)
-        
+
         last_input = src[:, -1, :][:, self.target_coin_index*self.output_features:(self.target_coin_index+1)*self.output_features]
         
         src = self.input_proj(src) * math.sqrt(self.hidden_dim)
@@ -72,22 +69,24 @@ class EncoderDecoderTransformer(nn.Module):
         # Initialize index 0 with last encoder input
         output[:, 0, :] = last_input
 
-        full_tgt_mask = self.generate_square_subsequent_mask(self.output_window + 1).to(src.device)
+        full_tgt_mask = nn.Transformer.generate_square_subsequent_mask(self.output_window + 1).to(src.device)
 
         for t in range(self.output_window):
-            decoder_input = output[:, :t+1+1, :].clone()
+            decoder_input = output[:, :t+1, :].clone()
+            
             decoder_input = self.output_proj(decoder_input) * math.sqrt(self.hidden_dim)
             decoder_input = self.pos_decoder(decoder_input)
 
             tgt_mask = full_tgt_mask[:t+1+1, :t+1+1]
 
-            out = self.transformer(src, decoder_input, tgt_mask=tgt_mask)
+            out = self.transformer(src, decoder_input, tgt_mask=tgt_mask, tgt_is_causal=True)
             out = self.output_linear(out)
 
             output[:, t+1, :] = out[:, -1, :]  # write to index t+1
 
         # Discard index 0 (the seed), return predictions only
-        return output[:, 1:, :].permute(0, 2, 1)
+        out = output[:, 1:, :].permute(0, 2, 1)
+        return out
 
     def set_teacher_forcing_ratio(self, new_value):
         self.teacher_forcing_ratio = new_value

train_scripts/model_classes/FeatureWiseCrossAttentionLSTM.py
@@ -44,14 +44,16 @@ class FeatureWiseCrossAttentionLSTM(nn.Module):
         group_outputs = []
         for i in range(self.num_groups):
             group = features_stack[:, i * self.output_features:(i + 1) * self.output_features]  # [batch, 4, hidden_dim]
-            attn_out, _ = self.group_attentions[i](group, group, group)
+            attn_mask = nn.Transformer.generate_square_subsequent_mask(self.num_groups).to(x.device)
+            attn_out, _ = self.group_attentions[i](group, group, group, attn_mask=attn_mask, is_causal=True)
             group_pooled = attn_out.mean(dim=1)
             group_outputs.append(group_pooled)
 
         groups_stack = torch.stack(group_outputs, dim=1)
 
         # apply attention to merged coin pipelines
-        final_attn_out, _ = self.final_attention(groups_stack, groups_stack, groups_stack)
+        attn_mask = nn.Transformer.generate_square_subsequent_mask(self.output_features).to(x.device)
+        final_attn_out, _ = self.final_attention(groups_stack, groups_stack, groups_stack, attn_mask=attn_mask, is_causal=True)
 
         # get the applied attention to the target coin pipeline output
         final_embedding = final_attn_out[:, self.target_coin_index, :]