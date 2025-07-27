f44d937cc6c036531ecd8b2e71b90382cd54bca2
mericdemirors <mericdemirors218@gmail.com>
2025-07-27 19:47:53 +0200

    changed additional loss folder naming

--- train_scripts/import_loss.py
+++ train_scripts/import_loss.py
@@ -4,7 +4,7 @@ def import_loss(loss_names):
     loss_functions = []
     for name in loss_names:
         try:
-            module = importlib.import_module(f'custom_losses.{name}')
+            module = importlib.import_module(f'additional_losses.{name}')
             loss_fn = getattr(module, name)
             loss_functions.append(loss_fn)
         except (ModuleNotFoundError, AttributeError) as e:
